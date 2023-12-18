6860496300a6a8272d21baa3513394c73330f0b8
Juan Sebastian Manitta <manittajuan@gmail.com>
2023-12-18 16:16:59 +0100

    first commit

---
+++ projects/lareferencia-widget-embed/src/lib/lareferencia-widget-embed.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class lareferenciaWidgetEmbedService {
+
+  private widgetConfigUrl = './assets/data/widget.config.json';
+
+  constructor(private http: HttpClient) { }
+
+  getWidgetConfig():Observable<any> {
+    return this.http.get(this.widgetConfigUrl);
+  }
+
+}
